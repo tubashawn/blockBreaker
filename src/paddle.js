fdb32260f27c0176a070b6f15f1c47c8145a77d4
tubaShawn <shawnrmangum@gmail.com>
2018-11-18 22:53:10 -0700

    paddle, non-functioning import

--- src/paddle.js
+++ src/paddle.js
@@ -1,16 +1,4 @@
-// can't get import to work...
-// import Paddle from "./src/paddle"; 
-
-
-let canvas = document.getElementById("gamescreen");
-let ctx = canvas.getContext("2d");
-
-ctx.clearRect(0, 0, 800, 600);
-
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-
-class Paddle {
+export default class Paddle {
     constructor(gameWidth, gameHeight) {
         this.width = 150;
         this.height = 30;
@@ -26,9 +14,3 @@ class Paddle {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
-
-
-
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-
-paddle.draw(ctx);
